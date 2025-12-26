5487bc2228e517b76b7febb74121eb7e2574909c
ThitiSo <thitis14443@gmail.com>
2025-12-26 07:08:21 +0700

    jinja1

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
-
+num =[1,2,3]
+news_items = {
+    1: {'id': 1, 
+        'title': 'COVID-19 update', 
+        'body': 'This is a news on COVID-19'},
+    2: {'id': 2, 
+        'title': 'Facemasks found', 
+        'body': 'Recent news on facemask production'},
+    3: {'id': 3,
+        'title':'Python 4', 
+        'body':'Python 4 will be out soon.... this is FAKE'},
+}
 @app.route("/")
 def hello_world():
     return "<p>Hello, NIG!</p>"
@@ -9,3 +20,9 @@ def hello_world():
 @app.route('/hello/')
 def hello_world1():
     return '<h1>Hi my name is Thiti<h2>'
+
+@app.route('/index/')
+def index():
+    # ...
+    return render_template('index.html', 
+                           news_items=news_items.values())
